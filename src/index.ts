dd56b5e2b71bb8a782445f46194153f45c617ef3
Richie Bendall <richiebendall@gmail.com>
2019-12-31 03:37:17 +1300

    refactor: Modularise classes
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -10,239 +10,243 @@ import EventEmitter from "events"
 import hexRgb from "hex-rgb"
 import _ from "lodash"
 
-const coords = ["x", "y"]
+namespace Mouse {
+    type mouseType = "left" | "right" | "middle"
 
-declare type mouseType = "left" | "right" | "middle"
+    const coords = ["x", "y"]
 
-class Mouse extends EventEmitter {
-    private delay_ = 10
+    const baseMouse = ["button", "clicks", ...coords]
 
-    public get delay(): number {
-        return this.delay_
-    }
-    public set delay(value: number) {
-        ow(value, ow.number)
-        this.delay_ = value
-        robot.setMouseDelay(value)
-    }
+    export class Mouse extends EventEmitter {
+        private _delay = 10
 
-    public move(x: number, y: number, { smooth = false, relative = false }: {
-        smooth?: boolean
-        relative?: boolean
-    } = {}): void {
-        ow(x, ow.number)
-        ow(y, ow.number)
-        ow(smooth, ow.boolean)
-        ow(relative, ow.boolean)
-
-        if (relative) {
-            x += this.x
-            y += this.y
+        constructor() {
+            super()
+            iohook.on("mousedown", (data) => this.emit("down", _.pick(data, baseMouse)))
+            iohook.on("mousemove", (data) => this.emit("move", _.pick(data, baseMouse)))
+            iohook.on("mouseclick", (data) => this.emit("click", _.pick(data, baseMouse)))
+            iohook.on("mousewheel", (data) => this.emit("wheel", _.pick(data, ["amount", "clicks", "direction", "rotation", ...coords])
+            ))
+            iohook.on("mousedrag", (data) => this.emit("wheel", _.pick(data, baseMouse)))
         }
 
-        if (smooth) robot.moveMouseSmooth(x, y)
-        else robot.moveMouse(x, y)
-    }
-
-    public dragTo(x: number, y: number, { relative = false }: {
-        relative?: boolean
-    } = {}): void {
-        ow(x, ow.number)
-        ow(y, ow.number)
-        ow(relative, ow.boolean)
-
-        if (relative) {
-            x += this.x
-            y += this.y
+        public get delay(): number {
+            return this._delay
+        }
+        public set delay(value: number) {
+            ow(value, ow.number)
+            this._delay = value
+            robot.setMouseDelay(value)
         }
 
-        robot.dragMouse(x, y)
-    }
+        public move(x: number, y: number, { smooth = false, relative = false }: {
+            smooth?: boolean
+            relative?: boolean
+        } = {}): void {
+            ow(x, ow.number)
+            ow(y, ow.number)
+            ow(smooth, ow.boolean)
+            ow(relative, ow.boolean)
 
-    public scrollTo(x: number, y: number, { relative = false }: {
-        relative?: boolean
-    } = {}): void {
-        ow(x, ow.number)
-        ow(y, ow.number)
-        ow(relative, ow.boolean)
+            if (relative) {
+                x += this.x
+                y += this.y
+            }
 
-        if (!relative) {
-            x -= this.x
-            y -= this.y
+            if (smooth) robot.moveMouseSmooth(x, y)
+            else robot.moveMouse(x, y)
         }
 
-        robot.scrollMouse(x, y)
-    }
+        public dragTo(x: number, y: number, { relative = false }: {
+            relative?: boolean
+        } = {}): void {
+            ow(x, ow.number)
+            ow(y, ow.number)
+            ow(relative, ow.boolean)
 
-    public down(button: mouseType = "left"): void {
-        ow(button, ow.string.is((val) => ["left", "right", "middle"].includes(val)))
-        robot.mouseToggle("down", button)
-    }
+            if (relative) {
+                x += this.x
+                y += this.y
+            }
 
-    public up(button: mouseType = "left"): void {
-        ow(button, ow.string.is((val) => ["left", "right", "middle"].includes(val)))
-        robot.mouseToggle("up", button)
-    }
+            robot.dragMouse(x, y)
+        }
 
-    public click(button: mouseType = "left") {
-        this.up(button)
-        this.down(button)
-        this.up(button)
-    }
+        public scrollTo(x: number, y: number, { relative = false }: {
+            relative?: boolean
+        } = {}): void {
+            ow(x, ow.number)
+            ow(y, ow.number)
+            ow(relative, ow.boolean)
 
-    public clickAt(x: number, y: number, button: mouseType = "left") {
-        this.move(x, y)
-        this.click(button)
-    }
+            if (!relative) {
+                x -= this.x
+                y -= this.y
+            }
 
-    public get x(): number {
-        return robot.getMousePos().x
-    }
+            robot.scrollMouse(x, y)
+        }
 
-    public get y(): number {
-        return robot.getMousePos().y
+        public down(button: mouseType = "left"): void {
+            ow(button, ow.string.is((val) => ["left", "right", "middle"].includes(val)))
+            robot.mouseToggle("down", button)
+        }
+
+        public up(button: mouseType = "left"): void {
+            ow(button, ow.string.is((val) => ["left", "right", "middle"].includes(val)))
+            robot.mouseToggle("up", button)
+        }
+
+        public click(button: mouseType = "left") {
+            this.up(button)
+            this.down(button)
+            this.up(button)
+        }
+
+        public clickAt(x: number, y: number, button: mouseType = "left") {
+            this.move(x, y)
+            this.click(button)
+        }
+
+        public get x(): number {
+            return robot.getMousePos().x
+        }
+
+        public get y(): number {
+            return robot.getMousePos().y
+        }
     }
 }
 
-const mouse = new Mouse()
+namespace Keyboard {
+    type modifier = "alt" | "command" | "control" | "shift"
 
-const baseMouse = ["button", "clicks", ...coords]
+    export class Keyboard extends EventEmitter {
+        public shortcut = new EventEmitter()
 
-iohook.on("mousedown", (data) => mouse.emit("down", _.pick(data, baseMouse)))
-iohook.on("mousemove", (data) => mouse.emit("move", _.pick(data, baseMouse)))
-iohook.on("mouseclick", (data) => mouse.emit("click", _.pick(data, baseMouse)))
-iohook.on("mousewheel", (data) => mouse.emit("wheel", _.pick(data, ["amount", "clicks", "direction", "rotation", ...coords])
-))
-iohook.on("mousedrag", (data) => mouse.emit("wheel", _.pick(data, baseMouse)))
+        private _delay = 10
 
-declare type modifier = "alt" | "command" | "control" | "shift"
+        constructor() {
+            super()
+            iohook.on("keypress", ({ keycode }) => this.emit("press", { key: kc(keycode), code: keycode }))
+            iohook.on("keydown", ({ keycode }) => this.emit("down", { key: kc(keycode), code: keycode }))
+            iohook.on("keyup", ({ keycode }) => this.emit("up", { key: kc(keycode), code: keycode }))
 
-class Keyboard extends EventEmitter {
-    public shortcut = new EventEmitter()
+            const keyboardListeners = {}
 
-    private delay_ = 10
+            this.shortcut.on("newListener", (combination: any, callback: Function) => {
+                if (_.isArray(combination)) keyboardListeners[iohook.registerShortcut(combination.map(cmb => _.isNumber(cmb) ? cmb : kc.codes[cmb]), callback)] = { combination, callback }
+            })
 
-    public get delay(): number {
-        return this.delay_
-    }
-
-    public set delay(value) {
-        ow(value, ow.number)
-        this.delay_ = value
-        robot.setKeyboardDelay(value)
-    }
-
-    public press(key: string, modifier: modifier | modifier[]): void {
-        ow(key, ow.string)
-        ow(modifier, ow.any(ow.undefined, ow.string.is(val => ["alt", "command", "control", "shift"].includes(val)), ow.array))
-
-        robot.keyTap(key, modifier)
-    }
-
-    public down(key: string, modifier: modifier | modifier[]): void {
-        ow(key, ow.string)
-        ow(modifier, ow.any(ow.undefined, ow.string.is(val => ["alt", "command", "control", "shift"].includes(val)), ow.array))
-
-        robot.keyToggle(key, "down", modifier)
-    }
-
-    public up(key: string, modifier: modifier | modifier[]): void {
-        ow(key, ow.string)
-        ow(modifier, ow.any(ow.undefined, ow.string.is(val => ["alt", "command", "control", "shift"].includes(val)), ow.array))
-
-        robot.keyToggle(key, "up", modifier)
-    }
-
-    public type(string: string, { interval }: {
-        interval?: number
-    }): void {
-        ow(string, ow.string)
-        ow(interval, ow.any(ow.undefined, ow.number))
-
-        if (_.isNumber(interval)) {
-            interval = string.length * interval / 60
+            this.shortcut.on("removeListener", (combination: any, callback: Function) => {
+                if (_.isArray(combination)) _.forOwn(keyboardListeners, ({ combination: cmb, callback: cb }: { combination: any, callback: Function }, key: string) => {
+                    if (
+                        cmb === combination &&
+                        cb === callback
+                    ) {
+                        iohook.unregisterShortcut(Number(key))
+                        return false
+                    }
+                })
+            })
         }
 
-        if (interval) robot.typeStringDelayed(string, interval)
-        else robot.typeString(string)
+        public get delay(): number {
+            return this._delay
+        }
+
+        public set delay(value) {
+            ow(value, ow.number)
+            this._delay = value
+            robot.setKeyboardDelay(value)
+        }
+
+        public press(key: string, modifier: modifier | modifier[]): void {
+            ow(key, ow.string)
+            ow(modifier, ow.any(ow.undefined, ow.string.is(val => ["alt", "command", "control", "shift"].includes(val)), ow.array))
+
+            robot.keyTap(key, modifier)
+        }
+
+        public down(key: string, modifier: modifier | modifier[]): void {
+            ow(key, ow.string)
+            ow(modifier, ow.any(ow.undefined, ow.string.is(val => ["alt", "command", "control", "shift"].includes(val)), ow.array))
+
+            robot.keyToggle(key, "down", modifier)
+        }
+
+        public up(key: string, modifier: modifier | modifier[]): void {
+            ow(key, ow.string)
+            ow(modifier, ow.any(ow.undefined, ow.string.is(val => ["alt", "command", "control", "shift"].includes(val)), ow.array))
+
+            robot.keyToggle(key, "up", modifier)
+        }
+
+        public type(string: string, { interval }: {
+            interval?: number
+        }): void {
+            ow(string, ow.string)
+            ow(interval, ow.any(ow.undefined, ow.number))
+
+            if (_.isNumber(interval)) {
+                interval = string.length * interval / 60
+            }
+
+            if (interval) robot.typeStringDelayed(string, interval)
+            else robot.typeString(string)
+        }
     }
 }
 
-const keyboard = new Keyboard()
+namespace Screen {
+    interface HexData {
+        red: number
+        green: number
+        blue: number
+        rgb: string
+        hex: string
+    }
 
-iohook.on("keypress", ({ keycode }) => keyboard.emit("press", { key: kc(keycode), code: keycode }))
-iohook.on("keydown", ({ keycode }) => keyboard.emit("down", { key: kc(keycode), code: keycode }))
-iohook.on("keyup", ({ keycode }) => keyboard.emit("up", { key: kc(keycode), code: keycode }))
-
-const keyboardListeners = {}
-
-keyboard.shortcut.on("newListener", (combination: any, callback: Function) => {
-    if (_.isArray(combination)) keyboardListeners[iohook.registerShortcut(combination.map(cmb => _.isNumber(cmb) ? cmb : kc.codes[cmb]), callback)] = { combination, callback }
-})
-
-keyboard.shortcut.on("removeListener", (combination: any, callback: Function) => {
-    if (_.isArray(combination)) _.forOwn(keyboardListeners, ({ combination: cmb, callback: cb }: { combination: any, callback: Function }, key: string) => {
-        if (
-            cmb === combination &&
-            cb === callback
-        ) {
-            iohook.unregisterShortcut(Number(key))
-            return false
+    export class Screen {
+        private betterHex(hex: string): HexData {
+            const { red, green, blue } = hexRgb(hex)
+            return {
+                red,
+                green,
+                blue,
+                rgb: `rgb(${red}, ${green}, ${blue})`,
+                hex: `#${hex}`
+            }
         }
-    })
-})
+
+        public pixelAt(x: number, y: number): HexData {
+            ow(x, ow.number)
+            ow(y, ow.number)
+
+            return this.betterHex(robot.getPixelColor(x, y))
+        }
+
+        public get width(): number {
+            return robot.getScreenSize().width
+        }
+
+        public get height(): number {
+            return robot.getScreenSize().height
+        }
+
+        public capture(x = 0, y = 0, width: number = screen.width, height: number = screen.height): robot.Bitmap {
+            ow(x, ow.number)
+            ow(y, ow.number)
+            ow(width, ow.number)
+            ow(height, ow.number)
+
+            return robot.screen.capture(x, y, width, height)
+        }
+    }
+}
 
 iohook.start(false)
 
-declare interface HexData {
-    red: number
-    green: number
-    blue: number
-    rgb: string
-    hex: string
-}
-
-class Screen {
-    private betterHex(hex: string): HexData {
-        const { red, green, blue } = hexRgb(hex)
-        return {
-            red,
-            green,
-            blue,
-            rgb: `rgb(${red}, ${green}, ${blue})`,
-            hex: `#${hex}`
-        }
-    }
-
-    public pixelAt(x: number, y: number): HexData {
-        ow(x, ow.number)
-        ow(y, ow.number)
-
-        return this.betterHex(robot.getPixelColor(x, y))
-    }
-
-    public get width(): number {
-        return robot.getScreenSize().width
-    }
-
-    public get height(): number {
-        return robot.getScreenSize().height
-    }
-
-    public capture(x = 0, y = 0, width: number = screen.width, height: number = screen.height): robot.Bitmap {
-        ow(x, ow.number)
-        ow(y, ow.number)
-        ow(width, ow.number)
-        ow(height, ow.number)
-
-        return robot.screen.capture(x, y, width, height)
-    }
-}
-
-const screen = new Screen()
-
-export = {
-    mouse,
-    keyboard,
-    screen,
-}
+export const mouse = new Mouse.Mouse()
+export const keyboard = new Keyboard.Keyboard()
+export const screen = new Screen.Screen()
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 import test from "ava"
-import theModule from "."
+import xoi from "."
 
 test("main", (t) => {
     t.throws(() => {
-        theModule(123)
+        xoi(123)
     }, {
         instanceOf: TypeError,
         message: "Expected a string, got number",
     })
 
-    t.is(theModule("unicorns"), "unicorns & rainbows")
+    t.is(xoi("unicorns"), "unicorns & rainbows")
 })
