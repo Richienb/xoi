bb1a8feb69c516b62e406588f0dcad75b971f34b
Richie Bendall <richiebendall@gmail.com>
2020-03-01 03:34:33 +1300

    refactor: Simplify keyboard logic
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import ow from "ow"
 import { EventEmitter } from "events"
 import _ from "lodash"
 import is from "@sindresorhus/is"
-import charcode from "charcode"
 import keycode from "keycode"
 
 // TODO: Blocked by https://github.com/wilix-team/iohook/pull/225
@@ -153,14 +152,14 @@ namespace Keyboard {
 
 		constructor() {
 			super()
-			iohook.on("keypress", ({ keychar: code, rawcode, shiftKey: shift, altKey: alt, ctrlKey: ctrl, metaKey: meta }) => this.emit("press", { key: charcode.from(rawcode), code, shift, alt, ctrl, meta }))
-			iohook.on("keydown", ({ keychar: code, shiftKey: shift, altKey: alt, ctrlKey: ctrl, metaKey: meta }) => this.emit("down", { key: keycode(code), code, shift, alt, ctrl, meta }))
-			iohook.on("keyup", ({ keychar: code, shiftKey: shift, altKey: alt, ctrlKey: ctrl, metaKey: meta }) => this.emit("up", { key: keycode(code), code, shift, alt, ctrl, meta }))
+			iohook.on("keypress", ({ keychar, rawcode: code, shiftKey: shift, altKey: alt, ctrlKey: ctrl, metaKey: meta }) => this.emit("press", { key: keycode(keychar), code, shift, alt, ctrl, meta }))
+			iohook.on("keydown", ({ rawcode: code, shiftKey: shift, altKey: alt, ctrlKey: ctrl, metaKey: meta }) => this.emit("down", { key: keycode(code), code, shift, alt, ctrl, meta }))
+			iohook.on("keyup", ({ rawcode: code, shiftKey: shift, altKey: alt, ctrlKey: ctrl, metaKey: meta }) => this.emit("up", { key: keycode(code), code, shift, alt, ctrl, meta }))
 
 			const keyboardListeners = {}
 
 			this.shortcut.on("newListener", (combination: any, callback: () => any) => {
-				if (is.array(combination)) keyboardListeners[iohook.registerShortcut(combination.map((cmb: string | number) => is.integer(cmb) ? cmb : kc.codes[cmb.toLowerCase()]), () => callback())] = { combination, callback }
+				if (is.array(combination)) keyboardListeners[iohook.registerShortcut(combination.map((cmb: string | number) => is.integer(cmb) ? cmb : keycode(cmb.toLowerCase())), () => callback())] = { combination, callback }
 			})
 
 			this.shortcut.on("removeListener", (combination: any, callback: () => any) => {
